0a2b9b3b0fd60b967cd1aff50aca1deb4e422cc5
TamariIv <taivgi@g.jct.ac.il>
2023-05-25 17:15:45 +0300

    add query class and update explainer

--- Explainer.py
+++ Explainer.py
@@ -1,18 +1,16 @@
 import openai
+import MakeQuery
+
 openai.api_key = "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"
 
 
-def get_explanation(query):
+def get_explanation(query: MakeQuery.Query):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": query},
-            {"role": "system", "content": "Answer shortly but clearly and explain unfamiliar terms. "
-                                          "The context is that you are getting a slide that is a part of a powerpoint"
-                                          "and you need to explain it."},
+            {"role": "user", "content": query.query},
+            {"role": "system", "content": query.context},
         ]
     )
     for item in completion.choices:
         print(item.message.content)
-
-
--- MakeQuery.py
+++ MakeQuery.py
@@ -1,3 +1,26 @@
+import SlideData
+
+
 def make_query(data):
     # will get a list of data
     pass
+
+
+class Query:
+    def __init__(self):
+        self.query = ''
+        self.context = ''
+
+    def from_slide(self, data: SlideData.SlideData) -> None:
+        self.context = "Getting the content of a slide that is a part of a powerpoint including the " \
+                       "titles and paragraphs in it. Please explain the slide. " \
+                       "Answers should be short but clear. Explain unfamiliar terms if needed."
+        q = ""
+        if data.titles:
+            q += "The titles in the slide are:\n"
+        for t in data.titles:
+            q += t + '\n'
+        if data.paragraphs:
+            q += "The paragraphs in the slide are:\n"
+        for p in data.paragraphs:
+            q += p + '\n'
