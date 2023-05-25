975edba026e33f7ba1c41718bc378ee6d944e15f
TamariIv <taivgi@g.jct.ac.il>
2023-05-25 13:15:54 +0300

    update powerpointReader and slideData

--- PowerpointReader.py
+++ PowerpointReader.py
@@ -9,11 +9,11 @@ class PowerpointReader:
         self.powerpoint = Presentation(_path)
         self.slides_iterator = iter(self.powerpoint.slides)
 
-    def read_slides(self):
+    def read_slides(self) -> list[SlideData.SlideData]:
+        slides_data = []
         for i, slide in enumerate(self.powerpoint.slides):
-            print("slide: ", i)
-            self.extract_data(slide)
-        print("end")
+            slides_data.append(self.extract_data(slide))
+        return slides_data
 
     def extract_data(self, slide: Slide) -> SlideData.SlideData:
         if not slide:
@@ -21,23 +21,9 @@ class PowerpointReader:
         titles, body = [], []
         for shape in slide.placeholders:
             if shape.placeholder_format.type in [1, 3]:
-                print("Title Placeholder:", shape.text)
+                titles.append(shape.text)
             elif shape.placeholder_format.type == 2:
-                print("Body Placeholder:", shape.text)
+                body.append(shape.text)
             elif shape.placeholder_format.type == 7 and shape.has_text_frame and shape.text:
-                print("Body Placeholder:", shape.text)
-
-        # for shape in slide.shapes:
-        #     for ph in slide.placeholders:
-        #         if ph.placeholder_format.type in [MSO_SHAPE_TYPE.]
-        #     # Check if the shape is a title placeholder
-        #     if shape.placeholder is not None and shape.placeholder.name == 'Title Placeholder':
-        #         title = shape.text
-        #         titles.append(title)
-        #     # Check if the shape is a text box
-        #     if shape.has_text_frame:
-        #         text = ''
-        #         for paragraph in shape.text_frame.paragraphs:
-        #             text += paragraph.text.strip()
-        #         body.append(text)
+                body.append(shape.text)
         return SlideData.SlideData(self.powerpoint.core_properties.title, titles, body)
--- SlideData.py
+++ SlideData.py
@@ -1,6 +1,8 @@
-
 class SlideData:
     def __init__(self, _ppt_title: str, _titles: list[str], _paragraphs: list[str]):
         self.ppt_title = _ppt_title
-        self.titles =_titles
+        self.titles = _titles
         self.paragraphs = _paragraphs
+
+    def slide_is_empty(self) -> bool:
+        return not (self.ppt_title and (self.titles or self.paragraphs))
