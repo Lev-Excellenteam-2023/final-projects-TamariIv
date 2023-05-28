89c7bf7ff45da84337b7bd2f6551ac3d523922f7
TamariIv <taivgi@g.jct.ac.il>
2023-05-28 22:58:48 +0300

    add json converting and test from main

--- JsonWriter.py
+++ JsonWriter.py
@@ -1,3 +1,7 @@
+import json
 
 
-
+def to_json(filename, *args):
+    with open(filename + ".json", "w") as file:
+        for arg in args:
+            json.dump(arg, file)
--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import collections
+import collections.abc
+import os
 import PowerpointReader
+import JsonWriter
 
 
 def main():
     path = r"C:\Users\tamar\Downloads\End of course exercise - kickof - upload.pptx"
     reader = PowerpointReader.PowerpointReader(path)
-    reader.read_slides()
+    data_list = reader.read_slides()
+    data = [
+        {
+            "name": "John Doe",
+            "age": 30,
+            "city": "New York"
+        },
+        {
+            "name": "John Dee",
+            "age": 23,
+            "city": "York"
+        },
+        {
+            "name": "MJ Doe",
+            "age": 34,
+            "city": "Seattle"
+        },
+    ]
+    JsonWriter.to_json(os.path.splitext(os.path.basename(path))[0], data)
     print("end")
 
 
