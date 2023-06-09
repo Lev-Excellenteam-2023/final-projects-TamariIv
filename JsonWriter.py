83fc6b6dc38717befc7fbb38b387752bae0aae8c
TamariIv <taivgi@g.jct.ac.il>
2023-06-09 09:20:19 +0300

    updadte writing to json

--- JsonWriter.py
+++ JsonWriter.py
@@ -3,5 +3,4 @@ import json
 
 def to_json(filename, *args):
     with open(filename + ".json", "w") as file:
-        for arg in args:
-            json.dump(arg, file)
+        json.dump(args, file, indent=4)
