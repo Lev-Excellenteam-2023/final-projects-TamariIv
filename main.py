d815cc68d79b86eca5d65341df69888caf0973dd
TamariIv <taivgi@g.jct.ac.il>
2023-06-29 23:58:57 +0300

    add web api parts

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ import PowerpointReader
 import JsonWriter
 import Explainer
 import MakeQuery
+import SystemTest
 
 
 async def main():
-    path = r"C:\Users\tamar\excellenteam\python_ex5\example.pptx"
-    reader = PowerpointReader.PowerpointReader(path)
-    data_list = reader.read_slides()
-    tasks = []
-    for sd in data_list:
-        q = MakeQuery.Query()
-        q.from_slide(sd)
-        task = asyncio.create_task(Explainer.get_explanation(q))
-        tasks.append(task)
-    explanations = await asyncio.gather(*tasks)
-    JsonWriter.to_json(os.path.splitext(os.path.basename(path))[0], *explanations)
-    print("end")
+    SystemTest.test_system()
+    # path = r"C:\Users\tamar\excellenteam\python_ex5\example.pptx"
+    # reader = PowerpointReader.PowerpointReader(path)
+    # data_list = reader.read_slides()
+    # tasks = []
+    # for sd in data_list:
+    #     q = MakeQuery.Query()
+    #     q.from_slide(sd)
+    #     task = asyncio.create_task(Explainer.get_explanation(q))
+    #     tasks.append(task)
+    # explanations = await asyncio.gather(*tasks)
+    # JsonWriter.to_json(os.path.splitext(os.path.basename(path))[0], *explanations)
+    # print("end")
 
 
 if __name__ == '__main__':
